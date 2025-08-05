Changed observation to be count-based vector instead of sequence, added action masking and fixed stability bonus

main.py
@@ -44,10 +44,13 @@ class ReplayMemory(object):
         return len(self.memory)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
+    print("Starting RL training...")
     ### SETUP RL LOOP ###
     env = get_env()
-    Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
+    Transition = namedtuple(
+        "Transition", ("state", "action", "next_state", "reward")
+    )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     BATCH_SIZE = 128
     GAMMA = 0.99
@@ -81,16 +84,26 @@ if __name__ == "main":
             -1.0 * steps_done / EPS_DECAY
         )
         steps_done += 1
+
+        # Use the environment's action masking function
+        action_mask = env._calculate_action_mask()
+        valid_actions = [i for i, valid in enumerate(action_mask) if valid]
+
         if sample > eps_threshold:
             with torch.no_grad():
-                # t.max(1) will return the largest column value of each row.
-                # second column on max result is index of where max element was
-                # found, so we pick action with the larger expected reward.
-                return policy_net(state).max(1)[1].view(1, 1)
+                q_values = policy_net(state)
+                # Mask invalid actions by setting their Q-values to -inf
+                mask = torch.tensor(
+                    action_mask, device=device, dtype=torch.bool
+                ).unsqueeze(0)
+                # Clone the Q-values and apply the mask
+                masked_q_values = q_values.clone()
+                masked_q_values[~mask] = float("-inf")
+                action = masked_q_values.max(1)[1].view(1, 1)
+                return action
         else:
-            return torch.tensor(
-                [[env.action_space.sample()]], device=device, dtype=torch.long
-            )
+            action = random.choice(valid_actions)
+            return torch.tensor([[action]], device=device, dtype=torch.long)
 
     def optimize_model():
         if len(memory) < BATCH_SIZE:
@@ -127,15 +140,19 @@ if __name__ == "main":
         # state value or 0 in case the state was final.
         next_state_values = torch.zeros(BATCH_SIZE, device=device)
         with torch.no_grad():
-            next_state_values[non_final_mask] = target_net(non_final_next_states).max(
-                1
-            )[0]
+            next_state_values[non_final_mask] = target_net(
+                non_final_next_states
+            ).max(1)[0]
         # Compute the expected Q values
-        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+        expected_state_action_values = (
+            next_state_values * GAMMA
+        ) + reward_batch
 
         # Compute Huber loss
         criterion = nn.SmoothL1Loss()
-        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+        loss = criterion(
+            state_action_values, expected_state_action_values.unsqueeze(1)
+        )
         # Optimize the model
         optimizer.zero_grad()
         loss.backward()
@@ -144,17 +161,24 @@ if __name__ == "main":
         optimizer.step()
 
     if torch.cuda.is_available():
-        num_episodes = 5000
+        num_episodes = 25000
     else:
         num_episodes = 50
 
     for i_episode in tqdm(range(num_episodes)):
         # Initialize the environment and get it's state
         state, info = env.reset()
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+        state = torch.tensor(
+            state, dtype=torch.float32, device=device
+        ).unsqueeze(0)
         for t in count():
             action = select_action(state)
-            observation, reward, terminated, truncated, info = env.step(action.item())
+            # Perform the action in the environment and get the next state,
+            # reward, and done flag
+            observation, reward, terminated, truncated, info = env.step(
+                action.item()
+            )
+            # Convert the observation to a tensor
             reward = torch.tensor([reward], device=device)
             if reward > 0:
                 logger.debug(f"Reward: {reward.item()}")
@@ -166,6 +190,7 @@ if __name__ == "main":
                 logger.debug(f"Tier: {info['current_tier']}")
                 logger.debug(f"Base Price: {info['current_base_price']}")
                 logger.debug(f"Cost: {info['current_cost']}")
+                logger.debug(f"Ingredients: {info['item_names']}")
             done = terminated or truncated
 
             if terminated:

potionomics_env/main.py
@@ -82,8 +82,12 @@ class PotionomicsEnvironment(gym.Env):
             )
         )
         self.num_ingredients[0] = 0  # Set the None action to have 0
-        self.current_ingredients: List[int] = []
+        # print(f"Number of Ingredients: {len(all_ingredients)}")
+        self.current_ingredients: np.ndarray = np.ascontiguousarray(
+            np.zeros(shape=(len(self.all_ingredients) + 1,), dtype=np.int_)
+        )
         self.current_stability = PotionomicsPotionStability.CANNOTMAKE
+        self.stability_rewards:List[float] = [0.0, 0.05, 0.5, 0.75, 1.0]
         self.potion_tier: PotionomicsPotionTier = None
         self.potion_traits: np.ndarray = np.zeros((5,))
         self.potion_magimin_thresholds_array = np.array(
@@ -345,44 +349,58 @@ class PotionomicsEnvironment(gym.Env):
         item_to_insert: PotionomicsIngredient = self._action_to_ingredient[
             index_of_item_to_insert
         ]
-        # logger.info(f"Attempting to Insert {item_to_insert.name}\tStock: {self.num_ingredients[index_of_item_to_insert]}")
-        has_enough_of_item: bool = self.num_ingredients[index_of_item_to_insert] > 0
+        logger.info(
+            f"Attempting to Insert {item_to_insert.name}\tStock: {self.num_ingredients[index_of_item_to_insert]}"
+        )
+        has_enough_of_item: bool = (
+            self.num_ingredients[index_of_item_to_insert] > 0
+        )
         magimin_check = (
             self.cauldron.current_total_magimin_amount
             + item_to_insert.total_magimin_value
             <= self.cauldron.max_total_magimin_allowed
         )
         if (
-            len(self.current_ingredients) < self.cauldron.max_num_items_allowed
+            self.current_ingredients.sum() < self.cauldron.max_num_items_allowed
             and has_enough_of_item
             and magimin_check
             and item_to_insert is not None
         ):
-            self.cauldron.current_a_magimin_amount += item_to_insert.a_magimin_value
-            self.cauldron.current_b_magimin_amount += item_to_insert.b_magimin_value
-            self.cauldron.current_c_magimin_amount += item_to_insert.c_magimin_value
-            self.cauldron.current_d_magimin_amount += item_to_insert.d_magimin_value
-            self.cauldron.current_e_magimin_amount += item_to_insert.e_magimin_value
+            self.cauldron.current_a_magimin_amount += (
+                item_to_insert.a_magimin_value
+            )
+            self.cauldron.current_b_magimin_amount += (
+                item_to_insert.b_magimin_value
+            )
+            self.cauldron.current_c_magimin_amount += (
+                item_to_insert.c_magimin_value
+            )
+            self.cauldron.current_d_magimin_amount += (
+                item_to_insert.d_magimin_value
+            )
+            self.cauldron.current_e_magimin_amount += (
+                item_to_insert.e_magimin_value
+            )
             self.cauldron.current_total_magimin_amount += (
                 item_to_insert.total_magimin_value
             )
             self.cauldron.current_num_items += 1
-            self.current_ingredients.append(index_of_item_to_insert)
+            self.current_ingredients[index_of_item_to_insert] += 1
             self.cost_of_items += item_to_insert.item_price
             self.num_ingredients[index_of_item_to_insert] -= 1
             # if self.num_ingredients[index_of_item_to_insert] < 1:
             #     logger.warning(f"{item_to_insert.name} has no more stock.")
             return 1
         else:
-            #             logger.debug(
-            #                 f"""
-            # Not Legal Because:
-            # Enough Space?: {len(self.current_ingredients) < self.cauldron.max_num_items_allowed}
-            # Enough of Item?: {has_enough_of_item}
-            # Passes Magimin Check?: {magimin_check}
-            # Item to Insert?: {item_to_insert}
-            # """
-            #             )
+            logger.debug(
+                f"""
+            Not Legal Because:
+            Enough Space?: {len(self.current_ingredients) < self.cauldron.max_num_items_allowed}
+            Enough of Item?: {has_enough_of_item}
+            Passes Magimin Check?: {magimin_check}
+            Item to Insert?: {item_to_insert}
+            """
+            )
             return 0
 
     def calculate_potion_rank_and_price(self) -> None:
@@ -406,10 +424,12 @@ class PotionomicsEnvironment(gym.Env):
             potion_index // self.potion_magimin_thresholds_array.shape[0]
         )
         row = potion_index // self.potion_magimin_thresholds_array.shape[0]
-        col = potion_index - (row * self.potion_magimin_thresholds_array.shape[0])
-        self.current_base_price = self.potion_prices_dict.get(self.recipe.name)[row][
-            col
-        ]
+        col = potion_index - (
+            row * self.potion_magimin_thresholds_array.shape[0]
+        )
+        self.current_base_price = self.potion_prices_dict.get(self.recipe.name)[
+            row
+        ][col]
 
     def calculate_current_magimin_ratios(self) -> np.ndarray:
         """Calculate the normalized magimin content of all ingredients in the cauldron.
@@ -485,7 +505,8 @@ class PotionomicsEnvironment(gym.Env):
             recipe_magimin_ratios - current_magimin_ratios
         )
         cumulative_delta: float = (
-            (self.recipe.magimin_ratios_int > 0).astype(np.int_) * magimin_deltas
+            (self.recipe.magimin_ratios_int > 0).astype(np.int_)
+            * magimin_deltas
         ).sum()
         if cumulative_delta >= 0.25:
             self.current_stability = PotionomicsPotionStability.CANNOTMAKE
@@ -503,7 +524,13 @@ class PotionomicsEnvironment(gym.Env):
         raise NotImplementedError()
 
     def _get_item_names_from_idx(self) -> List[str]:
-        return [self._action_to_ingredient[x].name for x in self.current_ingredients]
+        base_string: str = ""
+        for idx, ingredient in enumerate(self.all_ingredients):
+            if self.current_ingredients[idx] > 0:
+                base_string += (
+                    f"{ingredient.name} x{self.current_ingredients[idx]}\n"
+                )
+        return base_string
 
     def _get_current_ingredient_traits(self) -> np.ndarray:
         """Get the traits of all ingredients in the cauldron.
@@ -514,7 +541,7 @@ class PotionomicsEnvironment(gym.Env):
         """
 
         ingredients: List[int] = []
-        for ingredient_idx in self.current_ingredients:
+        for ingredient_idx in np.unique(self.current_ingredients):
             ingredient: PotionomicsIngredient = self._action_to_ingredient[
                 ingredient_idx
             ]
@@ -555,25 +582,13 @@ class PotionomicsEnvironment(gym.Env):
 
     def calculate_stability_bonus(self) -> float:
         """Calculate the stability bonus reward.
-
-        Uses the following equation:
-            `y = 1.661 * log_10(int(x) + 1e-8)`
-        Which would have the following values for each stability value:
-        |stability | x | y |
-        |:-:|:-:|:-:|
-        | CANNOTMAKE | 0 | -13.288    |
-        | UNSTABLE   | 1 |   7.214e-9 |
-        | STABLE     | 2 |   0.500    |
-        | VERYSTABLE | 3 |   0.793    |
-        | PERFECT    | 4 |   1.000    |
-
+        
         :return: A floating-point value that represents the bonus reward for
         creating high-stability potions.
         :rtype: float
         """
 
-        # The reason 1.661 is chosen is because we want Perfect ratio potions to give a reward of 1 (1.661*log_10(4) = 1)
-        return 1.661 * np.log10(int(self.current_stability) + 1e-8)
+        return self.stability_rewards[self.current_stability.value]
 
     def _calculate_reward_function(self) -> torch.Tensor:
         """Internal function that calculates the reward.
@@ -584,19 +599,81 @@ class PotionomicsEnvironment(gym.Env):
 
         cumulative_delta = torch.tensor(self.calculate_current_stability())
         reward = 0.0
-        if self.cauldron.current_num_items < 2:
-            return -1
+        if (
+            self.cauldron.current_num_items < 2
+            or self.current_stability == PotionomicsPotionStability.CANNOTMAKE
+        ):
+            return (
+                reward  # No reward for not having enough items in the cauldron
+            )
         else:
             self.calculate_potion_rank_and_price()
             # The higher stability, the better
             stability_bonus = self.calculate_stability_bonus()
+            logger.debug(
+                f"Reward calculation: (1.0 - cumulative_delta)={1.0 - cumulative_delta:.4f}, "
+                f"percent_full_magamin={self.cauldron.get_percent_full_magamin():.4f}, "
+                f"stability_bonus={stability_bonus:.4f}"
+            )
             reward = (
-                (1.0 - cumulative_delta) * (self.cauldron.get_percent_full_magamin())
-            ) + stability_bonus  # + price_vs_cost]
-            reward = float(np.clip(reward, -1, 1))
+                (1.0 - cumulative_delta)
+                * (self.cauldron.get_percent_full_magamin())
+            ) + stability_bonus  # + price_vs_cost
+            # reward = float(np.clip(reward, -1.0, 1.0))
         return reward
 
-    def reset(self, seed: Optional[int] = None, options: Dict[str, Any] = None) -> None:
+    def _calculate_action_mask(self) -> np.ndarray:
+        """Calculate the action mask for the agent.
+
+        The action mask is a binary array that indicates which actions are legal.
+        An action is legal if:
+            1. The item is not None
+            2. There is space in the cauldron for the item
+            3. The item does not exceed the remaining magimins available
+
+        :return: A binary array indicating which actions are legal.
+        :rtype: np.ndarray
+        """
+
+        action_mask = np.ones(len(self.all_ingredients) + 1, dtype=np.int_)
+        # Mask out actions (ingredients) that have a count of zero
+        action_mask[1:][self.num_ingredients[1:] == 0] = 0
+        # Check if the item can be inserted into the cauldron
+        # Get total magimin values, default to 0 if item is None
+        total_magimin_values = np.array(
+            [
+                item.total_magimin_value if item is not None else 0
+                for item in self.all_ingredients
+            ]
+        )
+        # Check magimin constraint
+        magimin_ok = (
+            self.cauldron.current_total_magimin_amount + total_magimin_values
+        ) <= self.cauldron.max_total_magimin_allowed
+        # Check ingredient count
+        enough_of_item = self.num_ingredients[1:] > 0
+        # Combine all conditions (skip index 0, which is always legal)
+        mask = magimin_ok & enough_of_item
+        action_mask[1:] = mask
+        # Check if the cauldron is full
+        if self.cauldron.is_full():
+            action_mask = np.zeros_like(action_mask, dtype=np.int_)
+        # Set the None action (index 0) to be legal
+        action_mask[0] = 1
+        return action_mask
+
+    @property
+    def action_mask(self) -> np.ndarray:
+        """Get the action mask for the agent.
+
+        :return: A binary array indicating which actions are legal.
+        :rtype: np.ndarray
+        """
+        return self._calculate_action_mask()
+
+    def reset(
+        self, seed: Optional[int] = None, options: Dict[str, Any] = None
+    ) -> None:
         """Reset the environment.
 
         :param seed: An integer that 'seeds' the random number generator, defaults to None
@@ -606,7 +683,9 @@ class PotionomicsEnvironment(gym.Env):
         """
 
         super().reset(seed=seed)
-        self.current_ingredients = []
+        self.current_ingredients: np.ndarray = np.ascontiguousarray(
+            np.zeros(len(self.all_ingredients) + 1, dtype=np.int_)
+        )
         self.current_stability = PotionomicsPotionStability.CANNOTMAKE
         self.potion_tier = None
         self.current_base_price = 0
@@ -633,7 +712,9 @@ class PotionomicsEnvironment(gym.Env):
 
     def step(
         self, action: int
-    ) -> Tuple[List[Union[int, float]], SupportsFloat, bool, bool, Dict[str, Any]]:
+    ) -> Tuple[
+        List[Union[int, float]], SupportsFloat, bool, bool, Dict[str, Any]
+    ]:
         """Perform a step in the environment.
 
         :param action: The action the agent selected. In this case, an action corresponds to an ingredient that the agent selects to put into the cauldron.
@@ -644,17 +725,23 @@ class PotionomicsEnvironment(gym.Env):
 
         terminated: bool = False
         legal_move: int = 1
-        if self._action_to_ingredient[action]:
+        ingredient_to_insert: PotionomicsIngredient = (
+            self._action_to_ingredient[action]
+        )
+        if ingredient_to_insert:
             legal_move = self.insert_item(index_of_item_to_insert=action)
         terminate_episode = (
-            (action is None) or (not legal_move) or (self.cauldron.is_full())
+            (ingredient_to_insert is None)
+            or (not legal_move)
+            or (self.cauldron.is_full())
         )
         if terminate_episode:
             terminated = True
         if legal_move:
             reward = self._calculate_reward_function()
         else:
-            reward = -1
+            logger.warning("Illegal Move!")
+            reward = -1.0
         observation = self._get_obs()
         info = self._get_info()
         return observation, reward, terminated, False, info
@@ -681,8 +768,10 @@ class PotionomicsEnvironment(gym.Env):
             "current_recipe": self.recipe,
             "item_names": self._get_item_names_from_idx(),
             "current_recipe_ratios": self.calculate_current_magimin_ratios(),
-            "current_stability": self.current_stability,
-            "current_tier": self.potion_tier,
+            "current_stability": self.current_stability.name,
+            "current_tier": (
+                self.potion_tier.name if self.potion_tier else "None"
+            ),
             "current_traits": self.potion_traits,
             "current_base_price": self.current_base_price,
             "current_cost": self.cost_of_items,
@@ -690,9 +779,13 @@ class PotionomicsEnvironment(gym.Env):
 
 
 def get_env() -> PotionomicsEnvironment:
-    potionomics_items_file = "./potionomics_env/data/potionomics_ingredients.csv"
+    potionomics_items_file = (
+        "./potionomics_env/data/potionomics_ingredients.csv"
+    )
     potionomics_recipes_file = "./potionomics_env/data/potionomics_recipes.csv"
-    potionomics_cauldrons_file = "./potionomics_env/data/potionomics_cauldrons.csv"
+    potionomics_cauldrons_file = (
+        "./potionomics_env/data/potionomics_cauldrons.csv"
+    )
     all_ingredients: List[PotionomicsIngredient] = []
     with open(potionomics_items_file, "r") as _file:
         csv_data = [line for line in csv.DictReader(_file)]